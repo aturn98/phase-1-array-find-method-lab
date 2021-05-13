f5b0fe951bed803e44512628e7914860654d973c
Austin <aturn9880@gmail.com>
2021-05-13 16:18:00 -0400

    commit tree 1d4f8dd46590188021f84d299574a0b12fbc10e3

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+function superbowlWin(target) {
+    const winningRecord = target.find((record) => record.result === 'W')
+    if (winningRecord) {
+        return winningRecord.year
+    } else {
+        return undefined
+    }
+}
 
